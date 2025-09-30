c3ea36de370bd0d09371afa023d5ea47f4bfd9f1
Luis Sarantes <sarantesluisjoel@gmail.com>
2025-09-29 19:08:18 -0600

    Primer commit de la guia, se agregaran todos los archivos creados por defecto de nest,ademas se crearon los model de user y Tenant, tambien este commit cubre hasta la migracion de la base de datos con prisma

---
+++ prisma/migrations/20250930000753_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "telephone" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
---
+++ prisma/migrations/20250930003326_add_tenant_model/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `tenantId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "tenantId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "public"."Tenant" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."User" ADD CONSTRAINT "User_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "public"."Tenant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
